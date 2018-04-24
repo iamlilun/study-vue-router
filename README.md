3d1b8c71fbaca100c460339c0f12ba1e9037e310
Lilun <iamlilun@taijun.net>
2018-04-24 21:15:21 +0800

    基本安裝設定

---
+++ README.md
@@ -0,0 +1,21 @@
+# lesson15
+
+> study vue-router
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# install vue-router
+npm i -S vue-router
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="menu">
+      <!--用 router-link 取代 a href-->
+      <router-link to="/about">About</router-link>
+      <router-link to="/products">Products</router-link>
+    </div>
+
+    <!--顯示View-->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import VueRouter from 'vue-router' //1. import vue-router
+import Products from './Products.vue'
+import About from './About.vue'
+
+export default {
+    router: new VueRouter({ //2. new
+        routes:[
+            {path: '/about', component: About},
+            {path: '/products', component: Products},
+        ],
+    }),
+}
+</script>
+
+<style>
+
+</style>
